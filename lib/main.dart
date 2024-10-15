410aeef629a4b84b2c8037af555e49aa69576624
matteo.faccetta.stud <matteo.faccetta.stud@ispascalcomandini.it>
2024-10-15 17:42:56 +0200

    fatta app per non visualizzare audio

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:audioplayers/audioplayers.dart';
+
 
 void main() {
   runApp(const ListenAudio());
@@ -10,12 +15,10 @@ class ListenAudio extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange,
-        brightness: Brightness.dark
-        )
-      ),
-      home: ListenerHomepage());
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+                seedColor: Colors.green, brightness: Brightness.light)),
+        home: const ListenerHomepage());
   }
 }
 
@@ -27,12 +30,57 @@ class ListenerHomepage extends StatefulWidget {
 }
 
 class _ListenerHomepageState extends State<ListenerHomepage> {
+  FilePickerResult? result; 
+  AudioPlayer audioPlayer = AudioPlayer();
+  double playRate=1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Audio Listener'),
       ),
+      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      body: Center(
+          child: Column(
+        children: [
+          Image.network(
+              "https://static.vecteezy.com/system/resources/previews/023/986/631/original/whatsapp-logo-whatsapp-logo-transparent-whatsapp-icon-transparent-free-free-png.png",
+              height: 300),
+          result == null ? const Text("scegli un vocale") : Text(result!.files.single.name),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                  onPressed: playAudio, icon: const Icon(Icons.play_arrow)),
+              TextButton(onPressed: playbackRate, child: Text('x$playRate')),
+            ],
+          )
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: pickFile,
+        child: const Icon(Icons.audio_file),
+      ),
     );
+  }
+
+  void pickFile() async {
+    result = await FilePicker.platform.pickFiles();
+    setState(() {
+      
+    });
+  }
+
+  void playAudio() {
+    if(result != null){
+      audioPlayer.play(DeviceFileSource(result!.files.single.path!));
+    }
+  }
+  void playbackRate() {
+    playRate = playRate == 1 ? 2 : 1;
+    audioPlayer.setPlaybackRate(playRate);
+    setState(() {
+      
+    });
   }
 }
