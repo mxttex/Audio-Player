dc90971ae224286f89f4d0b0eaf48bbbe42b6ed8
matteo.faccetta.stud <matteo.faccetta.stud@ispascalcomandini.it>
2024-10-20 20:23:02 +0200

    Aggiunta la possibilità di fermare l'audio, migliorati piccoli dettagli grafici

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:audioplayers/audioplayers.dart';
 
-
 void main() {
   runApp(const ListenAudio());
 }
@@ -30,15 +29,20 @@ class ListenerHomepage extends StatefulWidget {
 }
 
 class _ListenerHomepageState extends State<ListenerHomepage> {
-  FilePickerResult? result; 
+  FilePickerResult? result;
   AudioPlayer audioPlayer = AudioPlayer();
-  double playRate=1;
+  double playRate = 1;
+  bool playing = false;
+  Icon icon = Icon(Icons.play_arrow);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Audio Listener', style: TextStyle(fontSize: 25, fontStyle: FontStyle.italic, color: Colors.white)),
-        
+        title: const Text('Audio Listener',
+            style: TextStyle(
+                fontSize: 25,
+                fontStyle: FontStyle.italic,
+                color: Colors.white)),
       ),
       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       body: Center(
@@ -47,12 +51,14 @@ class _ListenerHomepageState extends State<ListenerHomepage> {
           Image.network(
               "https://static.vecteezy.com/system/resources/previews/023/986/631/original/whatsapp-logo-whatsapp-logo-transparent-whatsapp-icon-transparent-free-free-png.png",
               height: 300),
-          result == null ? const Text("Scegli un vocale") : Text(result!.files.single.name),
+          result == null
+              ? const Text("Scegli un vocale")
+              : Text(result!.files.single.name),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               IconButton(
-                  onPressed: playAudio, icon: const Icon(Icons.play_arrow)),
+                  onPressed: playAudio, icon: icon),
               TextButton(onPressed: playbackRate, child: Text('x$playRate')),
             ],
           )
@@ -61,30 +67,38 @@ class _ListenerHomepageState extends State<ListenerHomepage> {
       floatingActionButton: FloatingActionButton(
         onPressed: pickFile,
         child: const Icon(Icons.audio_file),
-        
       ),
     );
   }
 
   void pickFile() async {
-
     //il parametro initialDirectory funziona solo su android
-    result = await FilePicker.platform.pickFiles(type: FileType.audio,initialDirectory: "Memoria/Android/media/com.whatsapp/WhatsApp/Media/Whatsapp Voice Notes");
-    setState(() {
-      
-    });
+    result = await FilePicker.platform.pickFiles(
+        type: FileType.audio,
+        initialDirectory:
+            "Memoria/Android/media/com.whatsapp/WhatsApp/Media/Whatsapp Voice Notes");
+    setState(() {});
   }
 
   void playAudio() {
-    if(result != null){
+    if (result != null && !playing) {
       audioPlayer.play(DeviceFileSource(result!.files.single.path!));
+      setState(() {
+        icon = Icon(Icons.pause);
+        playing = true;
+      });
+    } else {
+      audioPlayer.pause();
+      setState(() {
+        icon = Icon(Icons.play_arrow);
+        playing = false;
+      });
     }
   }
+
   void playbackRate() {
     playRate = playRate == 1 ? 2 : 1;
     audioPlayer.setPlaybackRate(playRate);
-    setState(() {
-      
-    });
+    setState(() {});
   }
 }
